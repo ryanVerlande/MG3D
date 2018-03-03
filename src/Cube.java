ab186c5409fd3ea1dd1998e35d91db1a62291d1f
synave <synave@lisic.univ-littoral.fr>
2018-03-03 23:56:05 +0100

    Modification de la classe Cube Modification du nom de la classe Parallepipede en Parallelepipede

--- src/Cube.java
+++ src/Cube.java
@@ -1,40 +1,41 @@
 
-public class Cube extends Maillage {
+public class Cube extends Parallelepipede {
 
-	Maillage cube = new Maillage();
+    public Cube(){
+	super(new Point3D(0,0,0),1,1,1);
+    }
+    
+    public Cube(Point3D origine, double taille){
+	super(origine,taille,taille,taille);
+    }
+
+    public Cube(Cube c){
+    }
 	
-	public Cube(){
+    public void creationCube() {
+	cube.add(new Sommet(0, 0, 0));
+	cube.add(new Sommet(1, 0, 0));
+	cube.add(new Sommet(0, 1, 0));
+	cube.add(new Sommet(1, 1, 0));
+	cube.add(new Sommet(0, 0, 1));
+	cube.add(new Sommet(1, 0, 1));
+	cube.add(new Sommet(0, 1, 1));
+	cube.add(new Sommet(1, 1, 1));
 		
-	}
-	
-	public Cube(Sommet s1, Sommet s2, Sommet s3){
+	cube.add(new Face(3, 1, 0));
+	cube.add(new Face(2, 3, 0));
+	cube.add(new Face(3, 7, 1));
+	cube.add(new Face(5, 1, 7));
+	cube.add(new Face(6, 5, 7));
+	cube.add(new Face(5, 6, 4));
+	cube.add(new Face(4, 6, 2));
+	cube.add(new Face(4, 2, 0));
+	cube.add(new Face(6, 7, 3));
+	cube.add(new Face(3, 2, 6));
+	cube.add(new Face(1, 5, 0));
+	cube.add(new Face(4, 0, 5));
 		
-	}
-	
-	public void creationCube() {
-		cube.add(new Sommet(0, 0, 0));
-		cube.add(new Sommet(1, 0, 0));
-		cube.add(new Sommet(0, 1, 0));
-		cube.add(new Sommet(1, 1, 0));
-		cube.add(new Sommet(0, 0, 1));
-		cube.add(new Sommet(1, 0, 1));
-		cube.add(new Sommet(0, 1, 1));
-		cube.add(new Sommet(1, 1, 1));
-		
-		cube.add(new Face(3, 1, 0));
-		cube.add(new Face(2, 3, 0));
-		cube.add(new Face(3, 7, 1));
-		cube.add(new Face(5, 1, 7));
-		cube.add(new Face(6, 5, 7));
-		cube.add(new Face(5, 6, 4));
-		cube.add(new Face(4, 6, 2));
-		cube.add(new Face(4, 2, 0));
-		cube.add(new Face(6, 7, 3));
-		cube.add(new Face(3, 2, 6));
-		cube.add(new Face(1, 5, 0));
-		cube.add(new Face(4, 0, 5));
-		
-		cube.affichageSoupePolygone();
-	}
+	cube.affichageSoupePolygone();
+    }
 
 }
