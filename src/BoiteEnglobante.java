7aab5ed231d054c515600ed6fcab489550d52450
synave <synave@lisic.univ-littoral.fr>
2018-03-05 09:48:35 +0100

    Ajout de la classe BoiteEnglobante

---
+++ src/BoiteEnglobante.java
@@ -0,0 +1,38 @@
+class BoiteEnglobante{
+    private Point3D min, max;
+
+    public BoiteEnglobante(){
+	min = new Point3D(0,0);
+	max = new Point3D(0,0);
+    }
+
+    public BoiteEnglobante(Point3D mmin, Point3D mmax){
+	min = new Point3D(mmin);
+	max = new Point3D(mmax);
+    }
+
+    public BoiteEnglobante(Maillage m){
+	if(m.getNbSommets()==0)
+	    this();
+	else{
+	    min = new Point(m.getSommet(0).getX(),m.getSommet(0).getY(),m.getSOmmet(0).getZ());
+	    max = new Point(m.getSommet(0).getX(),m.getSommet(0).getY(),m.getSOmmet(0).getZ());
+	    for(int i=1;i<m.getNbSommets();i++){
+		if(m.getSommet(i).getX()<min.getX())
+		    min.setX(m.getSommet(i).getX());
+		if(m.getSommet(i).getX()>max.getX())
+		    max.setX(m.getSommet(i).getX());
+
+		if(m.getSommet(i).getY()<min.getY())
+		    min.setY(m.getSommet(i).getY());
+		if(m.getSommet(i).getY()>max.getY())
+		    max.setY(m.getSommet(i).getY());
+
+		if(m.getSommet(i).getZ()<min.getZ())
+		    min.setZ(m.getSommet(i).getZ());
+		if(m.getSommet(i).getZ()>max.getZ())
+		    max.setZ(m.getSommet(i).getZ());
+	    }
+	}
+    }
+}
