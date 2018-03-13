509f52a2983917712d7f386542fcdbc957151803
synave <synave@lisic.univ-littoral.fr>
2018-03-13 21:40:18 +0100

    Modification de Icosphere pour tenir compte de la nouvelle architecture

--- src/IcoSphere.java
+++ src/IcoSphere.java
@@ -1,64 +1,69 @@
-
 class IcoSphere extends Objet3D {
 
-	public IcoSphere() {
-		super();
-		
-		// sommet
-		add(new Sommet(-0.276385, -0.850640, -0.447215));
-		add(new Sommet(0.000000, 0.000000, -1.000000));
-		add(new Sommet(0.723600, -0.525720, -0.447215));
-		add(new Sommet(0.723600, 0.525720, -0.447215));
-		add(new Sommet(-0.894425, 0.000000, -0.447215));
-		add(new Sommet(-0.276385, 0.850640, -0.447215));
-		add(new Sommet(0.894425, 0.000000, 0.447215));
-		add(new Sommet(0.276385, -0.850640, 0.447215));
-		add(new Sommet(-0.723600, -0.525720, 0.447215));
-		add(new Sommet(-0.723600, 0.525720, 0.447215));
-		add(new Sommet(0.276385, 0.850640, 0.447215));
-		add(new Sommet(0.000000, 0.000000, 1.000000));
+    private Point3D centre;
+    private double rayon;
 
-		// face
-		add(new Face(0, 1, 2));
-		add(new Face(2, 1, 3));
-		add(new Face(4, 1, 0));
-		add(new Face(5, 1, 4));
-		add(new Face(3, 1, 5));
-		add(new Face(2, 3, 6));
-		add(new Face(0, 2, 7));
-		add(new Face(4, 0, 8));
-		add(new Face(5, 4, 9));
-		add(new Face(3, 5, 10));
-		add(new Face(6, 7, 2));
-		add(new Face(7, 8, 0));
-		add(new Face(8, 9, 4));
-		add(new Face(9, 10, 5));
-		add(new Face(10, 6, 3));
-		add(new Face(7, 6, 11));
-		add(new Face(8, 7, 11));
-		add(new Face(9, 8, 11));
-		add(new Face(10, 9, 11));
-		add(new Face(6, 10, 11));
+    public IcoSphere() {
+	super();
+	centre = new Point(0,0,0);
+	rayon=1;
+	
+	genereMaillage()
+    }
 
-		subdivision_faces();
+    public IcoSphere(Point3D ccentre, double rrayon) {
+	super();
+	centre = new Point3D(ccentre);
+	rayon=rrayon;
+	genereMaillage();
+    }
 
-		for (int i = 0; i < super.getNbSommets(); i++) {
-			Vecteur3D temp = new Vecteur3D(getSommet(i).getX(), getSommet(i).getY(), getSommet(i).getZ());
-			temp.normaliser();
-			getSommet(i).setX(temp.getDx());
-			getSommet(i).setY(temp.getDy());
-			getSommet(i).setZ(temp.getDz());
-		}
+    protected void genereMaillage(){
+	// sommet
+	add(new Sommet(-0.276385 * rayon + centre.getX(), -0.850640 * rayon + centre.getY(), -0.447215 * rayon + centre.getZ()));
+	add(new Sommet(0.000000 * rayon + centre.getX(), 0.000000 * rayon + centre.getY(), -1.000000 * rayon + centre.getZ()));
+	add(new Sommet(0.723600 * rayon + centre.getX(), -0.525720 * rayon + centre.getY(), -0.447215 * rayon + centre.getZ()));
+	add(new Sommet(0.723600 * rayon + centre.getX(), 0.525720 * rayon + centre.getY(), -0.447215 * rayon + centre.getZ()));
+	add(new Sommet(-0.894425 * rayon + centre.getX(), 0.000000 * rayon + centre.getY(), -0.447215 * rayon + centre.getZ()));
+	add(new Sommet(-0.276385 * rayon + centre.getX(), 0.850640 * rayon + centre.getY(), -0.447215 * rayon + centre.getZ()));
+	add(new Sommet(0.894425 * rayon + centre.getX(), 0.000000 * rayon + centre.getY(), 0.447215 * rayon + centre.getZ()));
+	add(new Sommet(0.276385 * rayon + centre.getX(), -0.850640 * rayon + centre.getY(), 0.447215 * rayon + centre.getZ()));
+	add(new Sommet(-0.723600 * rayon + centre.getX(), -0.525720 * rayon + centre.getY(), 0.447215 * rayon + centre.getZ()));
+	add(new Sommet(-0.723600 * rayon + centre.getX(), 0.525720 * rayon + centre.getY(), 0.447215 * rayon + centre.getZ()));
+	add(new Sommet(0.276385 * rayon + centre.getX(), 0.850640 * rayon + centre.getY(), 0.447215 * rayon + centre.getZ()));
+	add(new Sommet(0.000000 * rayon + centre.getX(), 0.000000 * rayon + centre.getY(), 1.000000 * rayon + centre.getZ()));
+
+	// face
+	add(new Face(0, 1, 2));
+	add(new Face(2, 1, 3));
+	add(new Face(4, 1, 0));
+	add(new Face(5, 1, 4));
+	add(new Face(3, 1, 5));
+	add(new Face(2, 3, 6));
+	add(new Face(0, 2, 7));
+	add(new Face(4, 0, 8));
+	add(new Face(5, 4, 9));
+	add(new Face(3, 5, 10));
+	add(new Face(6, 7, 2));
+	add(new Face(7, 8, 0));
+	add(new Face(8, 9, 4));
+	add(new Face(9, 10, 5));
+	add(new Face(10, 6, 3));
+	add(new Face(7, 6, 11));
+	add(new Face(8, 7, 11));
+	add(new Face(9, 8, 11));
+	add(new Face(10, 9, 11));
+	add(new Face(6, 10, 11));
+
+	subdivision_faces();
+
+	for (int i = 0; i < super.getNbSommets(); i++) {
+	    Vecteur3D temp = new Vecteur3D(getSommet(i).getX(), getSommet(i).getY(), getSommet(i).getZ());
+	    temp.normaliser();
+	    getSommet(i).setX(temp.getDx());
+	    getSommet(i).setY(temp.getDy());
+	    getSommet(i).setZ(temp.getDz());
 	}
-
-	public IcoSphere(Point3D centre, double rayon) {
-		this();
-		for (int i = 0; i < getNbSommets(); i++) {
-			getSommet(i).setX(getSommet(i).getX() * rayon);
-			getSommet(i).setY(getSommet(i).getY() * rayon);
-			getSommet(i).setZ(getSommet(i).getZ() * rayon);
-		}
-		translater(centre.getX(), centre.getY(), centre.getZ());
-	}
+    }
 
 }
